95c57061344e8673f28dd8ba3268e358c4a74457
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-08-13 00:09:47 -0300

    Change css

--- aula_03/header.php
+++ aula_03/header.php
@@ -3,10 +3,10 @@
             <aside>
                 <nav>
                   <h2>Menu</h2>
-                    <a href="?modulo=home" class="button"> <img src="" alt="">Home</a>
-                    <a href="?modulo=sobre" class="button">Sobre</a>
-                    <a href="?modulo=produtos" class="button">Produtos</a>
-                    <a href="?modulo=contato" class="button">Contato</a>
+                    <a href="?modulo=home" class="button"> <img src="./assets/images/home.svg" alt="Home icon"> Home</a>
+                    <a href="?modulo=sobre" class="button"><img src="./assets/images/sobre.svg" alt="Sobre icon"> Sobre</a>
+                    <a href="?modulo=produtos" class="button"><img src="./assets/images/produtos.svg" alt="Produto icon"> Produtos</a>
+                    <a href="?modulo=contato" class="button"><img src="./assets/images/contato.svg" alt="Contato icon"> Contato</a>
                 </nav>
             </aside>
 
