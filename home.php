03a44f0ef9cf161be543be2da40a3ea3aa6e549c
henrique-roldao <henriqueroldao@rede.ulbra.br>
2020-08-19 20:50:35 -0300

    xd

--- home.php
+++ home.php
@@ -44,7 +44,7 @@
                     <div class="column is-5 is-offset-1 ">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">1#</h2>
-                            <h1 class="title has-text-black is-3"> <img class="logo-langagues" src="./assets/img/python.svg" alt="Logo Python" width="48px" height="48px"> Python</h1>
+                            <h1 class="title has-text-black is-3"> <img class="logo-languages" src="./assets/img/python.svg" alt="Logo Python" width="48px" height="48px"> Python</h1>
                             <p class="has-text-dark">Python é a linguagem de programação mais quente para 2020. Como explica o
                                 IEEE Computer Society, a tecnologia continua sendo uma das melhores linguagens de programação já
                                 desenvolvidas. Python é eficiente no desenvolvimento de machine learning, inteligência artificial,
@@ -55,7 +55,7 @@
                     <div class="column is-5">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#2</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/kotlin.svg" alt="Logo Kotlin" width="48px" height="48px"> Kotlin</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/kotlin.svg" alt="Logo Kotlin" width="48px" height="48px"> Kotlin</h1>
                             <p class="has-text-dark">Conhecida por ser uma linguagem versátil, Kotlin foi desenvolvida pelo Android
                                 para ser operada ao lado, ou ao mesmo tempo que Java, no desenvolvimento de aplicações Android. No
                                 entanto, a Kotlin pode ser usada em qualquer outro projeto de interesse.</p>
@@ -71,7 +71,7 @@
                     <div class="column is-5 is-offset-1">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#3</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/java.svg" alt="Logo Java" width="48px" height="48px"> Java</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/java.svg" alt="Logo Java" width="48px" height="48px"> Java</h1>
                             <p class="has-text-dark">Tradicional, sólida e muito popular, Java faz seu 24° aniversário este ano.
                                 A linguagem orientada a objetos é tecnicamente mais fácil de aprender que linguagens como C e C++ e
                                 por esse motivo, mais de 90% das empresas selecionadas pela Fortune 500 dependem de Java para operar
@@ -83,7 +83,7 @@
                     <div class="column is-5">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#4</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/javascript.svg" alt="Logo Javascript" width="48px" height="48px"> Javascript</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/javascript.svg" alt="Logo Javascript" width="48px" height="48px"> Javascript</h1>
                             <p class="has-text-dark">Por ser uma linguagem versátil, rápida e atualizada anualmente, o JavaScritp
                                 pode ser utilizado mesmo no desenvolvimento front-end como no back-end. Por esse motivo, o JavaScript
                                 pode ser encontrado em aplicações de grandes empresas de tecnologia como a Netflix, Uber, PayPal e
@@ -100,7 +100,7 @@
                     <div class="column is-5 is-offset-1">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#5</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/typescript.svg" alt="Logo TypeScript" width="48px" height="48px"> TypeScript</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/typescript.svg" alt="Logo TypeScript" width="48px" height="48px"> TypeScript</h1>
                             <p class="has-text-dark">TypeScript é uma das linguagens de programação que mais cresce, de acordo com o
                                 GitHub. Além de já ser uma das mais populares da atualidade. Desenvolvida pela Microsoft, o TypeScript
                                 se baseia em JavaScript, o que permite que as duas linguagens sejam usadas de forma integrada, no mesmo código.</p>
@@ -110,7 +110,7 @@
                     <div class="column is-5">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#6</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/golang.svg" alt="Logo Go" width="48px" height="48px"> Go</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/golang.svg" alt="Logo Go" width="48px" height="48px"> Go</h1>
                             <p class="has-text-dark">Go é uma linguagem de programação nova, que combina aspectos da programação
                                 funcional, com estilos orientados a objetos. É a linguagem que mais cresce no Github destinada a
                                 substituir linguagens como Java e C++.</p>
@@ -126,7 +126,7 @@
                     <div class="column is-5 is-offset-1">
                         <div class="content is-medium">
                             <h2 class="subtitle is-5 has-text-grey">#7</h2>
-                            <h1 class="title has-text-black is-3"><img class="logo-langagues" src="./assets/img/swift.svg" alt="Logo Swift" width="48px" height="48px"> Swift</h1>
+                            <h1 class="title has-text-black is-3"><img class="logo-languages" src="./assets/img/swift.svg" alt="Logo Swift" width="48px" height="48px"> Swift</h1>
                             <p class="has-text-dark">Swift é uma linguagem dedicada para o desenvolvimento de aplicações para os
                                 sistemas iOS e MacOS, da Apple. A linguagem oferece a seus desenvolvedores uma sintaxe simples e
                                 funcional, profundamente influenciada por Python e Ruby.
